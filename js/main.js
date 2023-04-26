c9b53c2fb93cc2d12833e5471313b18bb04367e9
NGOQUANGKHOA <20521468@gm.uit.edu.vn>
2023-04-26 22:32:05 +0700

    final update

---
+++ js/main.js
@@ -0,0 +1,182 @@
+$(document).ready(function() {
+    // process bar
+    setTimeout(function() {
+        firstQuestion();
+        $('.spinner').fadeOut();
+        $('#preloader').delay(350).fadeOut('slow');
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    }, 600);
+})
+
+function init() {
+    document.getElementById('titleWeb').innerHTML = CONFIG.titleWeb
+    $('#title').text(CONFIG.title)
+    $('#desc').text(CONFIG.desc)
+    $('#yes').text(CONFIG.btnYes)
+    $('#no').text(CONFIG.btnNo)
+
+    var xYes = (0.9 * $(window).width() - $('#yes').width() - $('#no').width()) / 2;
+    var xNo = xYes + $('#yes').width() + 0.1 * $(window).width();
+    var y = 0.75 * $(window).height();
+    $('#yes').css("left", xYes);
+    $('#yes').css("top", y);
+
+    $('#no').css("left", xNo);
+    $('#no').css("top", y);
+}
+
+function init2() {
+    document.getElementById('titleWeb').innerHTML = CONFIG.titleWeb
+    $('#title').text(CONFIG.title2)
+    $('#desc').text(CONFIG.desc2)
+    $('#yes').text(CONFIG.btnYes2)
+    $('#no').text(CONFIG.btnNo2)
+
+    var xYes = (0.9 * $(window).width() - $('#yes').width() - $('#no').width()) / 2;
+    var xNo = xYes + $('#yes').width() + 0.1 * $(window).width();
+    var y = 0.75 * $(window).height();
+    $('#yes').css("left", xYes);
+    $('#yes').css("top", y);
+
+    $('#no').css("left", xNo);
+    $('#no').css("top", y);
+}
+
+function firstQuestion() {
+    $('.content').hide();
+    Swal.fire({
+        title: CONFIG.introTitle,
+        // text:CONFIG.introDesc,
+        html: `<p>Trời lạnh là tại đông</p>
+               <p>Yêu nàng là tại hạ</p>
+               <p>From Ngô Quang Khoa with love <3</p> `,
+        imageUrl: 'img/love.gif',
+        imageWidth: 300,
+        imageHeight: 300,
+        background: '#fff url("img/iput-bg.jpg")',
+        imageAlt: 'Custom image',
+        confirmButtonText: CONFIG.btnIntro
+    }).then(function() {
+        $('.content').show(200);
+    })
+}
+
+// switch button position
+function switchButton() {
+    var leftNo = $('#no').css("left");
+    var topNO = $('#no').css("top");
+    var leftY = $('#yes').css("left");
+    var topY = $('#yes').css("top");
+    $('#no').css("left", leftY);
+    $('#no').css("top", topY);
+    $('#yes').css("left", leftNo);
+    $('#yes').css("top", topNO);
+}
+// move random button position
+function moveButton() {
+    var x = Math.random() * ($(window).width() - $('#no').width()) * 0.9;
+    var y = Math.random() * ($(window).height() - $('#no').height()) * 0.9;
+    var left = x + 'px';
+    var top = y + 'px';
+    $('#no').css("left", left);
+    $('#no').css("top", top);
+}
+
+init()
+
+var n = 0;
+$('#no').mousemove(function() {
+    if (Math.random() < 0.5 || n == 1)
+        switchButton();
+    else
+        moveButton();
+    n++;
+});
+$('#no').click(() => {
+    if (screen.width >= 900)
+        switchButton();
+})
+
+// generate text in input
+function textGenerate() {
+    var n = "";
+    var text = " " + CONFIG.reply;
+    var a = Array.from(text);
+    var textVal = $('#txtReason').val() ? $('#txtReason').val() : "";
+    var count = textVal.length;
+    if (count > 0) {
+        for (let i = 1; i <= count; i++) {
+            n = n + a[i];
+            if (i == text.length + 1) {
+                $('#txtReason').val("");
+                n = "";
+                break;
+            }
+        }
+    }
+    $('#txtReason').val(n);
+    setTimeout("textGenerate()", 1);
+}
+var flat = 0;
+// show popup
+$('#yes').click(function() {
+    if (flat == 0) {
+        init2();
+        $('.content').hide();
+        Swal.fire({
+            title: CONFIG.question1,
+            text: '(Định chọn không chứ giề 🙄🙄)',
+            background: '#fff url("img/iput-bg.jpg")',
+            confirmButtonColor: '#3085d6',
+            confirmButtonColor: '#fe8a71',
+            confirmButtonText: CONFIG.btnReply1
+        }).then(function() {
+            $('.content').show(200);
+        })
+        flat = 1;
+        setTimeout(function() {
+            $('.spinner').fadeOut();
+            $('#preloader').delay(350).fadeOut('slow');
+            $('body').delay(350).css({
+                'overflow': 'visible'
+            });
+        }, 600);
+    }
+    else {
+    var audio = new Audio('sound/tick.mp3');
+    audio.play();
+    Swal.fire({
+        title: CONFIG.question2,
+        html: true,
+        width: 900,
+        padding: '3em',
+        html: "<input type='text' class='form-control' id='txtReason' placeholder='Mẫu câu trả lời: mùa hạ...'>",
+        background: '#fff url("img/iput-bg.jpg")',
+        backdrop: `
+              rgba(0,0,123,0.4)
+              url("img/logi.gif")
+              left top
+              no-repeat
+            `,
+        confirmButtonColor: '#3085d6',
+        confirmButtonColor: '#fe8a71',
+        confirmButtonText: CONFIG.btnReply2
+    }).then((result) => {
+        if (result.value) {
+            Swal.fire({
+                width: 900,
+                confirmButtonText: CONFIG.btnAccept,
+                background: '#fff url("img/iput-bg.jpg")',
+                title: CONFIG.mess,
+                text: CONFIG.messDesc,
+                confirmButtonColor: '#83d0c9',
+                onClose: () => {
+                    window.location = CONFIG.messLink;
+                }
+            })
+        }
+    })
+    }
+})
